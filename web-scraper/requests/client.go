d4ae2762159b63e36521cb5d69c067ecc2a5fa48
TonyCallaghan <a.tc.callaghan@gmail.com>
2023-07-20 18:52:48 +0100

    Dockerize project

--- web-scraper/requests/client.go
+++ web-scraper/requests/client.go
@@ -4,21 +4,33 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"github.com/tonycallaghan/web-scraper/core"
+	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/tonycallaghan/web-scraper/core"
 )
 
 func PostPlant(plant core.Plant) error {
 	plantJSON, _ := json.Marshal(plant)
 
-	resp, err := http.Post("http://localhost:8080/plant", "application/json", bytes.NewBuffer(plantJSON))
+	resp, err := http.Post("http://api:8080/plant", "application/json", bytes.NewBuffer(plantJSON))
+
 	if err != nil {
 		return err
 	}
 
 	defer resp.Body.Close()
 
-	if resp.StatusCode != 200 {
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Println("Error reading response body:", err)
+		} else {
+			bodyString := string(bodyBytes)
+			log.Println("Response status code:", resp.StatusCode)
+			log.Println("Response body:", bodyString)
+		}
 		return errors.New("status code error")
 	}
 
